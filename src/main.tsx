fe63f6a823607690d6d7efc75f7ff5f879a84835
Bidhut Karki <bidhutkarki@meta.com>
2026-07-10 18:41:39 -0700

    Add toaster for sign up success, tailwind css and chakra v3

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import { ChakraProvider, defaultSystem } from '@chakra-ui/react'
 import './index.css'
 import App from './App.tsx'
+import { Toaster } from './shared/toaster'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ChakraProvider value={defaultSystem}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      <Toaster />
+    </ChakraProvider>
   </StrictMode>,
 )
--- src/modules/users/pages/SignupPage.tsx
+++ src/modules/users/pages/SignupPage.tsx
@@ -1,12 +1,22 @@
 import { useNavigate } from "react-router-dom";
 import { SignupForm } from "../components/SignupForm";
+import { toaster } from "../../../shared/toaster";
 
 export function SignupPage() {
   const navigate = useNavigate();
 
   return (
     <div className="game">
-      <SignupForm onRegistered={() => navigate("/")} />
+      <SignupForm
+        onRegistered={(user) => {
+          toaster.create({
+            title: `Welcome, ${user.firstName}!`,
+            description: "Your account was created.",
+            type: "success",
+          });
+          navigate("/");
+        }}
+      />
     </div>
   );
 }
